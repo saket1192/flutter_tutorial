2705e55901ed391151030ef6ffd7f9febd6c4ccd
Saket Kumar <saket@phtl.com>
2021-05-15 20:57:40 +0530

    theme extracted

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:tutorial_flutter/Pages/login_page.dart';
 import 'package:tutorial_flutter/utils/routes.dart';
 import 'package:tutorial_flutter/widgets/themes.dart';
